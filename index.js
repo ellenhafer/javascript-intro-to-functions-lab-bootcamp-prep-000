f8975fc55f2199b68d2d30283e067ab20238cd5d
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-05-23 12:33:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string === lowercase) {
+  if(string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  else if(string === lowercase) {
     return "I can't hear you!"
   }
   else if(string === uppercase) {
